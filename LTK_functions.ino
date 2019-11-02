925ed1105002444220e90446a7fe643f48e07a22
huynhtancuong <huynhtancuong.hcm@gmail.com>
2019-11-02 12:59:35 +0700

    third commit

--- LTK_functions.ino
+++ LTK_functions.ino
@@ -1,7 +1,16 @@
+#include "GRobotDevice.h"
+
+bool phatHienVatCan(int port, int khoang_cach = 30) {
+  if (GetUltrasonicDistance(port) < khoang_cach) {
+    return 1;
+  }
+  return 0;
+}
+
 void LTK_setup() {
   Serial.println("Serial connected as baudrate 9600");
 }
 
 void LTK_loop() {
-  Serial.println("");
+  if (phatHienVatCan(5, 40)) Serial.println("");
 }
